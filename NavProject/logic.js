13a09e3c7823fcff579ceccd1e614be72618ddb9
Alex Samaniego <alex.samaniego@ppiweb.com>
2017-05-15 17:16:32 +0000

    Create Radial Menu

---
+++ NavProject/logic.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+    $(".circle li:nth-child(1)").hover(function(){
+        
+        $(".center").text("Embracing personal responsibility to act in anticipation of perceived problems and opportunities while fully owning actions, behaviors, and outcomes.")
+        
+    });
+    
+     $(".circle li:nth-child(2)").hover(function(){
+        
+        $(".center").text("Simple behavioral, technical, and organizational aids for rapidly growing Reliability, Efficiency, Productivity, and Safety.")
+        
+    });
+    
+    $(".circle li:nth-child(3)").hover(function(){
+        
+        $(".center").text("Building relationships while aligning attitudes and actions with organizational Core Principles.")
+        
+    });
+    
+     $(".circle li:nth-child(4)").hover(function(){
+        
+        $(".center").text("Sharing, understanding, and acting upon insights gained from organizational activities, conditions, and outcomes.")
+        
+    });
+    
+    
+ var text = ["Embracing personal responsibility to act in anticipation of perceived problems and opportunities while fully owning actions, behaviors, and outcomes.","Simple behavioral, technical, and organizational aids for rapidly growing Reliability, Efficiency, Productivity, and Safety.", "Building relationships while aligning attitudes and actions with organizational Core Principles.", "Sharing, understanding, and acting upon insights gained from organizational activities, conditions, and outcomes."]
+    
+ 
+     
+ });
+    
+    
+});
