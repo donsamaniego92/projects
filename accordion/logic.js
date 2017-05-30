60e48cbc3a436857e3b13c65a3e07357a11a38d9
Alex Samaniego <alex.samaniego@ppiweb.com>
2017-05-30 15:02:42 +0000

    Final change to working Accordion

--- accordion/logic.js
+++ accordion/logic.js
@@ -1,130 +1,139 @@
-$(document).ready(function(){
-    
-    $(".cont").click(function(){
-        
-         
-        
-        $("#bas1").toggle();
-        
-       $(this).find("span").toggleClass("glyphicon-minus");
-       
-       
-       
-        $("#bas2").css('display', 'none');
-        
-        $("#bas3").css('display', 'none');
-        
-      
-                    
-      
-        
-    })
-    
-    $(".cont2").click(function(){
-        
-        $("#bas2").toggle();
-        
-       $(this).find("span").toggleClass("glyphicon-minus");
-        
-    $("#bas1").css('display', 'none');
-    
-     $("#bas3").css('display', 'none');
-        
-    })
-    
-    
-    $(".cont3").click(function(){
-        
-        $("#bas3").toggle();
-        
-       $(this).find("span").toggleClass("glyphicon-minus");
-        
-    $("#bas1").css('display', 'none');
-    
-    $("#bas2").css('display', 'none');
-        
-    })
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+$(document).ready(function() {
 
-    
-    $(".cont").mouseover(function(){
-        
-        $("#tit1").css('color',"white");
-        
-        $("#im1").css('color','white')
-  
-        
-        
+    // Container 1 
+
+    $(".cont").click(function() {
+
+        $("#bas1").toggle();
+
+        $("#bas2").css('display', 'none');
+
+        $("#bas3").css('display', 'none');
+
+
+        $(".cont").find('span').toggleClass('glyphicon-minus')
+
+
+
+        $(".cont2").find('span').attr("class", 'glyphicon glyphicon-plus')
+
+        $(".cont3").find('span').attr("class", 'glyphicon glyphicon-plus')
+
     })
-    
-    $(".cont").mouseleave(function(){
-        
-        $("#tit1").css('color',"#828490");
-        
-        $("#im1").css('color','#828490')
-       
-        
+
+    // Container 2 
+
+
+    $(".cont2").click(function() {
+
+        $("#bas2").toggle();
+
+        $("#bas1").css('display', 'none');
+
+        $("#bas3").css('display', 'none');
+
+
+
+        $(".cont2").find('span').toggleClass('glyphicon-minus')
+
+        $(".cont").find('span').attr("class", 'glyphicon glyphicon-plus')
+
+        $(".cont3").find('span').attr("class", 'glyphicon glyphicon-plus')
+
+
+        // Container 3 
+
+
     })
-    
-    $(".cont2").mouseover(function(){
-        
-        $("#tit2").css('color',"white");
-        
-        $("#im2").css('color','white')
-        
-        
-        
+
+
+    $(".cont3").click(function() {
+
+        $("#bas3").toggle();
+
+        $("#bas1").css('display', 'none');
+
+        $("#bas2").css('display', 'none');
+
+
+
+        $(".cont3").find('span').toggleClass('glyphicon-minus')
+
+        $(".cont").find('span').attr("class", 'glyphicon glyphicon-plus')
+
+        $(".cont2").find('span').attr("class", 'glyphicon glyphicon-plus')
+
+
+
+
     })
-    
-    $(".cont2").mouseleave(function(){
-        
-        $("#tit2").css('color',"#828490");
-        
-        $("#im2").css('color','#828490')
-       
-        
+
+
+    // Mouseover Container 1
+
+    $(".cont").mouseover(function() {
+
+        $("#tit1").css('color', "white");
+
+        $("#im1").css('color', 'white')
+
+
+
     })
-    
-    $(".cont3").mouseover(function(){
-        
-        $("#tit3").css('color',"white");
-        
-        $("#im3").css('color','white')
-        
-        
-        
+    // Mouseleave Container 1
+
+    $(".cont").mouseleave(function() {
+
+        $("#tit1").css('color', "#828490");
+
+        $("#im1").css('color', '#828490')
+
+
     })
-    
-    $(".cont3").mouseleave(function(){
-        
-        $("#tit3").css('color',"#828490");
-        
-        $("#im3").css('color','#828490')
-       
-        
+
+    // Mouseover Container 2
+
+    $(".cont2").mouseover(function() {
+
+        $("#tit2").css('color', "white");
+
+        $("#im2").css('color', 'white')
+
+
+
     })
-    
-    
+    // Mouseleave Container 2
+
+    $(".cont2").mouseleave(function() {
+
+        $("#tit2").css('color', "#828490");
+
+        $("#im2").css('color', '#828490')
+
+
+    })
+
+    // Mouseover Container 3
+
+    $(".cont3").mouseover(function() {
+
+        $("#tit3").css('color', "white");
+
+        $("#im3").css('color', 'white')
+
+
+
+    })
+    // Mouseleave Container 3
+
+    $(".cont3").mouseleave(function() {
+
+        $("#tit3").css('color', "#828490");
+
+        $("#im3").css('color', '#828490')
+
+
+    })
+
+
 })
