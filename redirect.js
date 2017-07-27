68d7ab23ded1cac8272ff9c08aec515dc7aa1d73
Alex Samaniego <alex.samaniego@ppiweb.com>
2017-07-27 21:24:07 +0000

    Start and Finish Capstone Project

---
+++ redirect.js
@@ -0,0 +1,16 @@
+<script>
+
+setTimeout(function () { 
+    
+    window.location.href = "https://www.google.com";
+}
+
+</script>
+
+alert('hi');
+
+for(var start = 1; start < 10; start++) {
+  setTimeout(function () {
+    alert('hello');
+  }, 3000);
+}
